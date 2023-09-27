ffe7303107e1105c7549b574507c1c1512be420d
TUSHAR292004 <tusharchandak2004@gmail.com>
2023-09-27 13:18:13 +0530

    datatypes tutorial

---
+++ 01_basic/02_Datatypes.js
@@ -0,0 +1,13 @@
+"use strict" ;  //treats all JS code as newr version 
+
+/* 
+number => 2 to power 53
+bigint
+string => '' or ""
+boolean => true or flase 
+null => standalone value 
+undefined => not assigned a value yet
+object => { }
+array => [ ]
+symbol (new in ES6)
+*/
