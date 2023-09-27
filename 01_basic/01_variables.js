5e8eb202f4abb6e0aa93a1bf57c12b18abf52103
TUSHAR292004 <tusharchandak2004@gmail.com>
2023-09-27 13:16:50 +0530

    varibales tutorial

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,9 @@
+const userName = "Tushar" ; // not changeable after declaration
+let userEmail = "tusharchandak2004@gmail.com" ;  // it is blocked scoped and can be re assign 
+var userAge =  19 ;  // isuues related block and functional scope not to use 
+userLocation = "Durg" // javascript will automatiocally detect it 
+
+// non valued but declared variables value will be undefined 
+ 
+console.log(userName , userEmail , userAge, userLocation);
+console.table([userName , userEmail , userAge, userLocation]);
