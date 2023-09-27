7ec06a97008a80205c8c32a6d10988bef21d8cbf
TUSHAR292004 <tusharchandak2004@gmail.com>
2023-09-27 13:58:20 +0530

    conversion

---
+++ 01_basic/03_Conversion.js
@@ -0,0 +1,15 @@
+let score = "22abc"
+console.log(typeof score); //String
+
+let scoreInt = Number(score) //conversion to Number  
+console.log(typeof scoreInt); //number
+console.log(scoreInt); //   NaN not a number 
+
+/*
+In Booleand conversion 
+1 => true
+0 => false 
+"" => false
+"tushar" => true
+ */
+
