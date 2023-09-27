ba30f1c8703a89b8026971ac4be4b38c333707ca
TUSHAR292004 <tusharchandak2004@gmail.com>
2023-09-27 13:25:06 +0530

    datatypes

--- 01_basic/02_Datatypes.js
+++ 01_basic/02_Datatypes.js
@@ -1,13 +1,24 @@
 "use strict" ;  //treats all JS code as newr version 
 
-/* 
+/*  PRIMITIVE DATAYPES 
 number => 2 to power 53
 bigint
 string => '' or ""
 boolean => true or flase 
 null => standalone value 
 undefined => not assigned a value yet
+symbol (new in ES6) =>  used for uniqueness
+
+    NON PRIMITIVE DATA TYPE 
 object => { }
 array => [ ]
-symbol (new in ES6)
+
 */
+
+const rollNo  = 14.00;  // number 
+let studentName = "Tushar" //String 
+
+console.log(typeof rollNo);   
+//typeof is used to get the datatyp of the variable 
+
+// NOTE : null is in Object and undefined is its own type 
