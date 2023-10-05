37dc131bd1bb0a4797c2624ab841e9474576e509
TUSHAR292004 <tusharchandak2004@gmail.com>
2023-10-06 00:24:23 +0530

    basic function

---
+++ 03_functions/01_function.js
@@ -0,0 +1,11 @@
+// defining function
+
+// here a and b are parameters
+function myFunction(a, b) {
+   return `Sum of ${a} and ${b} is ${a + b}`; 
+   // after this code will be unreachable 
+}
+//function execution / reference is : myFunctoion
+const result  = myFunction(3, 5) // here 3 , 5 are arguments  
+console.log(result);
+
