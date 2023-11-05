85bd0af678c3a5d4375509582edf750913126450
TUSHAR292004 <tusharchandak2004@gmail.com>
2023-11-06 01:14:28 +0530

    Control Flow

--- 05_control_flow/if_flow.js
+++ 05_control_flow/if_flow.js
@@ -6,6 +6,13 @@ if (userLoggedIn && debitCard && 2==3) {
     console.log("Not found");
 }
 
+const loggedInFromGoogle = true ;
+const loggedInFromEmail = false ;
+
+if( loggedInFromEmail || loggedInFromGoogle){
+    console.log('User is Logged In');
+}
+
 const userEmail = [] ;  //truthy value 
 if (userEmail) {
     console.log(userEmail);
@@ -31,3 +38,19 @@ if (Object.keys(userName).length === 0) {
 // false == 0 : true 
 // false == '' : true
 // 0 == '' : true
+
+//Nullish Coalescing Operator (??) : null undefined
+let val1 ;
+val1  = 5 ?? 10 
+console.log(val1);
+val1 = null ?? 10; // null got eliminated (safety check used for databases)
+console.log(val1);
+val1 = undefined ?? 15 ; // undefined got eliminated 
+console.log(val1);
+val1 = null ?? 10 ?? 15; //1st value will be assign 
+console.log(val1);
+
+// Terniary Operator 
+// condition ? true : false 
+const iceTeaPrice = 100;
+iceTeaPrice <= 80 ? console.log("Cheap") : console.log("Costly");
