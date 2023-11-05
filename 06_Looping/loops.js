1a8e033921e4673a9315638f7fd48d1b9008c35a
TUSHAR292004 <tusharchandak2004@gmail.com>
2023-11-06 01:59:56 +0530

    loops

--- 06_Looping/loops.js
+++ 06_Looping/loops.js
@@ -1,7 +1,8 @@
 //for loop 
-// for (let index = 0; index < array.length; index++) {
-//    console.log(array[index]);   
-// }
+const array1 = [1,2,3,4,5]
+for (let index = 0; index < array1.length; index++) {
+   console.log(array1[index]);   
+}
 
 //break & continue
 const array = [1,4,5,7,0,9]
@@ -14,3 +15,18 @@ for (let index = 0; index < array.length; index++) {
     }
     console.log(array[index]);
 }
+
+//while 
+let index = 0;
+while (index < 5) {
+    console.log("Hayee");
+    index ++ ;
+}
+
+//Do While 
+do {
+    console.log('Hello');
+    index ++ ;
+}
+while (index < 0)
+
