5850ac6b64639433a0e3343877f1a5a95b3ca21b
TUSHAR292004 <tusharchandak2004@gmail.com>
2023-11-06 02:33:13 +0530

    Advance Loooping

---
+++ 06_Looping/Arr_loops.js
@@ -0,0 +1,56 @@
+// forof 
+const arr = [1,2,3,4,5] ;
+for (const i of arr) {
+    console.log(i);
+}
+
+// Maps  : always have unique values and not iterable forin will not work
+const map = new Map()
+map.set('name', 'John')
+map.set('age', 30)
+map.set('name', 'John')
+console.log(map)
+
+for (const [iterator, value] of map) {
+    console.log(iterator, " :- ", value);
+}
+
+// forin 
+const obj1 = {
+    js: "javascript",
+    py: "python",
+    cpp: "c++"
+}
+for (const key in obj1) {
+    console.log(key , " : ", obj1[key]);
+}
+
+//for each  uses call back func whichi dont have name 
+let array = ['a','b','c'];
+array.forEach((value, index)=>{
+    console.log(`${index}: ${value}`);
+})
+
+let array2 = [1,2,3,4,5]
+function print(i){
+    console.log(i);
+}
+array2.forEach(print); // just giving the refference of function not executing it 
+
+const myCoding = [
+ {
+    langName : "JavaScript",
+    saveFile : "js"
+ },
+ {
+    langName : "Python",
+    saveFile : "py"
+ },
+ {
+    langName : "Java",
+    saveFile : "java"
+ }   
+]
+myCoding.forEach((item) => {
+    console.log(item, item.langName, item.saveFile);
+})
